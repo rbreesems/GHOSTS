9b38b172d5ba735625068ead47470f46b8f05d3e
Bob Reese <rbreesems@gmail.com>
2026-05-04 15:19:21 -0500

    Added support for Edge Browser in Windows

--- src/Ghosts.Domain/Messages/Timeline.cs
+++ src/Ghosts.Domain/Messages/Timeline.cs
@@ -120,6 +120,7 @@ namespace Ghosts.Domain
         PowerShell = 39,
         Bash = 40,
         Print = 45,
+        BrowserEdge = 46,
         Ssh = 100,
         Sftp = 101,
         Pidgin = 102,
